7437ec3401d34ac31cc8c19705f2ba1c5c353299
Ankit Saraswat <152775694+saraswatAnkit@users.noreply.github.com>
2024-10-27 13:40:06 +0530

    first commit

---
+++ basic/variables.js
@@ -0,0 +1,25 @@
+// name = "ankit"
+// console.log(name);
+//  let | const | var
+
+// let - used for declaring the variable in the local scope
+// var - used for declaring the variables in the global scope
+// const - used for declaring the constant value in the local scope
+
+// let name = "ankit"
+// console.log(name);
+// {
+//     let name = "harshit"
+// }
+// console.log(name);
+
+// var name = "sharma"
+// {
+//     var name = "ankush"
+// }
+// console.log(name);
+
+const name = "ankit"
+// name = "harshit"
+console.log(name);
+
